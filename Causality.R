c52d47b4cf13346c5e318fe97e305ef004dc2908
J4nn2s <jannesglaubitz@gmail.com>
2024-03-05 11:45:34 +0100

    First Exercise

--- Causality.R
+++ Causality.R
@@ -54,5 +54,60 @@ ATNT = mean(pension$indiTreat[pension$p401==0])
 
 
 
+# 2. Aufgabe
+
+# Probabilty that someone took part on p401 plan
+
+probit_Fit = glm(formula = p401 ~ age + db + educ + fsize + hown + inc + male + marr
+                 + pira + twoearn,
+                 data = pension,
+                 family = binomial(link = "probit"))
+summary(probit_Fit)
+
+pension$pScore = predict(probit_Fit, type = "response")
+
+
+
+
+member = pension[pension$inc == 48252, "pScore"]
+
+pension$pScoreDiff = ifelse(pension$p401 == 0, abs(pension$pScore-member), NA)
+
+pensionNew = na.omit(pension)
+
+# Most similar means in this case minimal difference in pScoreDiff
+individual = pensionNew[pensionNew$pScoreDiff==min(pensionNew$pScoreDiff),]
+individual$pScore
+
+
+'''
+Hier ist tricky, dass na.omit zwar die NAs entfernt, aber die dennoch bleiben diese na
+gespeichert. Das erste Element, ist das wonach wir suchen, deshalb einfach [1]
+'''
+
+delta = na.omit(ifelse(pension$inc==48252, pension$net_tfa - individual$net_tfa,NA))
+delta
+
+
+
+delta = ifelse(pension$inc == 48252, pension$net_tfa - individual$net_tfa, NA)
+na.omit(delta)[1]
+delta[1]
+
+# Load package
+library(MatchIt)
+# Matching step
+mod = matchit(formula = p401 ~ age + db + educ + fsize + hown + inc + male + marr + pira
+              + twoearn,
+              data = pension,
+              method = "nearest",
+              link ="probit",
+              replace =TRUE)
+matches = get_matches(mod)
+# Regression step
+fitmatch = lm(net_tfa ~ p401,
+              data = matches,
+              weights = matches$weights)
+fitmatch$coefficients
 
 
